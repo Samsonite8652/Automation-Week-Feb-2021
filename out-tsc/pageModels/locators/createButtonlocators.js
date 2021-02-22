4c53fc1856eb542b0bd3379f6f36f0602e3cf11c
Simataa <simataa.sam@mayo.edu>
2021-02-22 04:09:54 -0600

    Adding the out-tsc to the gitignore file

--- out-tsc/pageModels/locators/createButtonlocators.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createButtonLocators = void 0;
-exports.createButtonLocators = {
-    "createButtonId": "#createRoom"
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY3JlYXRlQnV0dG9ubG9jYXRvcnMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9wYWdlTW9kZWxzL2xvY2F0b3JzL2NyZWF0ZUJ1dHRvbmxvY2F0b3JzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFhLFFBQUEsb0JBQW9CLEdBQUc7SUFDaEMsZ0JBQWdCLEVBQUUsYUFBYTtDQUNsQyxDQUFDIn0=
